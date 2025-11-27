4368306ad9cba35a0f25f5cae34c2a7b0f8226e6
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-11-27 11:09:35 +0530

    Find common between two arrays.

--- 02-DSA_Love_Babbar/02-Arrays/Lect._09-10/10-Intersection_Elements.js
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._09-10/10-Intersection_Elements.js
@@ -25,7 +25,32 @@ async function ask(q) {
 // 01a. A function to find count or frequency of an element in an array.
 function findCommonElements(arr1, arr2) {
   let i = 0;
+  let j = 0;
   const commonElements = [];
+
+  // Sorting both arrays.
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  // While loop will only run till either of the array is completely traversed.
+  while (i < arr1.length && j < arr2.length) {
+    // if array-1 element is bigger, jump onto next index of the array-2 using j++.
+    if (arr1[i] > arr2[j]) {
+      j++;
+    }
+
+    // Similarly, if array-2 element is bigger, jump onto next index of the array-1 using i++.
+    if (arr2[j] > arr1[i]) {
+      i++;
+    }
+
+    // if array-1 element is equal to array-2, then push the element to the commonElements array.
+    if (arr1[i] == arr2[j]) {
+      commonElements.push(arr1[i]);
+      i++;
+      j++;
+    }
+  }
 
   return commonElements;
 }
@@ -34,12 +59,12 @@ function findCommonElements(arr1, arr2) {
 
 // 01b. Taking user input:
 const inputArray1 = await ask(
-  "Enter an array elements separated by a single space: "
+  "Enter an array-1 elements separated by a single space: "
 );
 const arr1 = inputArray1.trim().split(" ").map(Number);
 
 const inputArray2 = await ask(
-  "Enter an array elements separated by a single space: "
+  "Enter an array-2 elements separated by a single space: "
 );
 const arr2 = inputArray2.trim().split(" ").map(Number);
 // -----------------------------
@@ -48,9 +73,9 @@ const arr2 = inputArray2.trim().split(" ").map(Number);
 const commonElements = findCommonElements(arr1, arr2);
 
 console.log(
-  `Common elements between array-1: ${arr1} and array-2: ${arr2} are: ${[
+  `Common elements between array-1: ${arr1} and array-2: ${arr2} are: [${[
     ...commonElements,
-  ]}`
+  ]}]`
 );
 
 rl.close(); // This will close the CLI for the user input.
