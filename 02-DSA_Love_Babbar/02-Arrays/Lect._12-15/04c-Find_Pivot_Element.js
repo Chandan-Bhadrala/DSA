ddb0645f16cf490edd911f007d2c0fb8071e558d
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-02 14:33:03 +0530

    Setting up file for finding the maximum pivot value in the rotated sorted array.

---
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/04c-Find_Pivot_Element.js
@@ -0,0 +1,32 @@
+// # Find the pivot element in a rotated and sorted array.
+// 1. Now find the maximum pivot value in the rotated sorted array.
+
+//-----------------
+
+// 01. A function to find the pivot element in the rotated sorted array.
+function pivotElement(arr) {
+  
+    let start = 0;
+    let end = arr.length - 1;
+    let mid = Math.floor((start + end) / 2);
+
+    while (start < end) {
+        // Updating mid-pointer value.
+        mid = Math.floor((start + end) / 2);
+
+        // We will be shifting start and the end pointer towards the unsorted section of the array, in pursuit to find the minimum value, which is our pivot value.
+
+        // Shifting start-pointer towards the right.
+        if (arr[mid] > arr[end]) {
+            start = mid + 1;
+        }
+
+        // Shifting end-pointer towards the left.
+        else if (arr[mid] <= arr[end]) {
+            end = mid;
+        }
+    }
+
+    // start === end -> index of minimum (pivot)
+    return arr[end];
+}
