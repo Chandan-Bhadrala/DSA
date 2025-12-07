e4f9ee349838e864285cbd492ef6422262c881b4
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-07 21:57:41 +0530

    Book Allocation Problem. Done.

--- 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/07-Book_Allocation.js
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/07-Book_Allocation.js
@@ -7,45 +7,73 @@
 // 3.1. Thus, no student shall be given book pages randomly, but students shall be allotted book pages in a contiguous manner.
 
 //-----------------
+// ## Solution Approach:
+// 1. Create a number range starting from 0 to the sum of all the book pages.
+// 1.1. Use binary search to calculate the mid for the above range.
+// 2. Pass this mid value to a helper function to check whether this mid value represents the maximum minimum average pages that can be allotted to a student or not.
+// 3. If yes, store this mid value as an answer.
+// 3.1. Then recalculate the mid again by shrinking the boundaries by changing "end = mid - 1".
+// 3.2. Further check if there is another possible reduced value of mid which can represent the value of maximum minimum average pages that can be allotted to a student or not.
+// 3.3. If found, repeat the search again for a further reduced value, as in step 2.
+// 4. If the current "mid" value doesn't represent a value that can be allotted to students, then shift the "start = mid + 1" and repeat step 2.
+// 5. Steps 3 and 4 will repeat until the binary search space is exhausted with the help of the while loop condition "start <= end".
 
-// 01. A function to search for a given element in the rotated sorted array.
-function pivotElement(arr, searchN) {
-  let start = 0;
-  let end = arr.length - 1;
-  let mid = null;
+//-----------------
+
+// 01a. A function to search for a mid value that can represent the maximum possible book pages that can be allotted to a student, which in contrast is the minimum possible book pages that have to be allotted to a student.
+function allocateBooksPages(arr, students) {
+  // Early return.
+  if (students > arr.length || students <= 0) {
+    return -1; // As number of students are greater than number of books, then one book can't be assigned to each student. or if the given number of students are equal to or less than zero.
+  }
+
+  // Keeping start value instead of zero, rather equal to the book with maximum page size, as the book allocation can't be smaller than the book size of the one book which has maximum number of pages.
+  let start = Math.max(...arr);
+  let end = arr.reduce((acc, current) => acc + current, 0);
+  let finalResult = -1;
 
   while (start <= end) {
-    // Updating mid-pointer value.
-    mid = Math.floor((start + end) / 2);
+    let mid = Math.floor((start + end) / 2);
 
-     //00. If the element is found at mid.
-    if (arr[mid] === searchN) return mid;
+    // Calling the isPossible function to check whether the given mid value represents the maximum possible pages that can be allotted to a student without violating the condition that no student must get more than the maximum possible book pages.
+    if (isPossible(arr, mid, students)) {
+      // If true, then store the recent possible answer given by the mid in a variable.
+      finalResult = mid;
 
-    // 1. Selecting either of the sorted side of the array for searching the target value.
-
-    // 1.1. Below condition assures us, the target element is in the left sorted array half.
-    if (searchN > arr[end]) {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end] && searchN > arr[mid]) start = mid + 1;
-      else if (arr[mid] < arr[end]) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-    // 1.2. Below condition assures us, the target element is in the right sorted array half.
-    else {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end]) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] < searchN) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] > searchN) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
+      // Now, further reduce the search space to the left, to check for another reduced possible mid value.
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
 
-  // Didn't found the target value within the array.
-  return -1;
+  // Returning the found answer for the maximum possible number of book pages that can be alloted to a student, which in contrast is a minimum possible book pages.
+  return finalResult;
+}
+
+// 01b. isPossible, a helper function to validate the viability of the mid value for a possible answer.
+function isPossible(arr, mid, students) {
+  let studentCount = 1;
+  let pageSum = 0;
+  let i = 0;
+
+  // Traversing the whole array.
+  while (i < arr.length) {
+    // Early return upon finding the book pages to be greater than mid.
+    if (arr[i] > mid) {
+      return false;
+    }
+
+    if (pageSum + arr[i] <= mid) {
+      pageSum += arr[i];
+    } else {
+      // If the current student has reached its maximum possible pages, then bring up the next student and hand him over the next set of pages.
+      studentCount++;
+      if (studentCount > students) return false;
+      pageSum = arr[i];
+    }
+    // Bring up the next book and its pages.
+    i++;
+  }
+  return true;
 }
