3577e5c59e5e071b3c1338ae356645298b178c54
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-09 00:48:27 +0530

    Setting up files for PRAT ROTI and Beautiful Triplets.

--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
@@ -1,54 +1,13 @@
-// # Search for an element in a rotated and sorted array.
-// 1. Will be calculating pivot value first in this approach.
-// 1.1. And then will confirm at which side the target value lies and will scan for the target value in that range later using binary search.
-
-// Didn't tried this approach.
-// Happily learned about the fact that:
-// 1. Compare the start and the end pointer with the **mid pointer**.
-// 1.1. Role of the **mid-pointer**, is only of the comparison with the start or the end pointer, to make the right decision of shrinking the boundaries.
-// 2. In the unsorted array, always first find which half is sorted and only use the boundary pointer of the sorted half of the array to compare with it with the mid pointer, to come to the decision of shifting the start or the end pointer.
-// 2.1. Start and the end pointers are considered as the boundary pointers.
-// 2.1.1. Because they define the search space boundary.
+// # PRATA ROTI
+// A beautiful description and the solution for this problem can be found on the below url.
+// https://copyassignment.com/roti-prata-spoj-problem-solution/
 //-----------------
 
-// 01. A function to search for a given element in the rotated sorted array.
-function pivotElement(arr, searchN) {
+// 01. A function to solve for PRATA ROTI problem.
+function minimumTimeRequired(arr, searchN) {
   let start = 0;
   let end = arr.length - 1;
   let mid = null;
 
-  while (start <= end) {
-    // Updating mid-pointer value.
-    mid = Math.floor((start + end) / 2);
-
-     //00. If the element is found at mid.
-    if (arr[mid] === searchN) return mid;
-
-    // 1. Selecting either of the sorted side of the array for searching the target value.
-
-    // 1.1. Below condition assures us, the target element is in the left sorted array half.
-    if (searchN > arr[end]) {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end] && searchN > arr[mid]) start = mid + 1;
-      else if (arr[mid] < arr[end]) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-    // 1.2. Below condition assures us, the target element is in the right sorted array half.
-    else {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end]) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] < searchN) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] > searchN) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-  }
-
-  // Didn't found the target value within the array.
-  return -1;
+  
 }
--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/12-Beautiful_Triplets.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/12-Beautiful_Triplets.js
@@ -1,54 +1,23 @@
-// # Search for an element in a rotated and sorted array.
-// 1. Will be calculating pivot value first in this approach.
-// 1.1. And then will confirm at which side the target value lies and will scan for the target value in that range later using binary search.
+// # Beautiful Triplets.
+// 1. In this question we will be given an array with the numbers in the increasing orders, we have to find count of beautiful triplets.
+// Q) What is beautiful triplets.
+// 1. If we have three numbers x, y, z.
+// 1.1. Then for these three numbers to be a beautiful triplets:
+// x - y == z - y == d.
+// 2. In question, you'll be given the value of "d" and you'd have to find the numbers in the array that satisfy the condition or difference of value "d".
 
-// Didn't tried this approach.
-// Happily learned about the fact that:
-// 1. Compare the start and the end pointer with the **mid pointer**.
-// 1.1. Role of the **mid-pointer**, is only of the comparison with the start or the end pointer, to make the right decision of shrinking the boundaries.
-// 2. In the unsorted array, always first find which half is sorted and only use the boundary pointer of the sorted half of the array to compare with it with the mid pointer, to come to the decision of shifting the start or the end pointer.
-// 2.1. Start and the end pointers are considered as the boundary pointers.
-// 2.1.1. Because they define the search space boundary.
+//-----------------
+// Solution Approach:
+// 1. Pick first value at arr[0] and take "d" and compute value of "y" and "z".
+// 2. Search the array for the "y" and "z", if found these values of "y" and "z" in the array then, increment count variable by 1.
+// 3. If no value for "y" and "z", found move to arr[1] and compute again value of "y" and "z" with the help of the "d" and then search for the value of "y" and "z" again as in the "step 2".
 //-----------------
 
-// 01. A function to search for a given element in the rotated sorted array.
-function pivotElement(arr, searchN) {
+// 01. A function to search for a beautiful triplets.
+function beautifulTriplets(arr, difference) {
   let start = 0;
   let end = arr.length - 1;
   let mid = null;
 
-  while (start <= end) {
-    // Updating mid-pointer value.
-    mid = Math.floor((start + end) / 2);
-
-     //00. If the element is found at mid.
-    if (arr[mid] === searchN) return mid;
-
-    // 1. Selecting either of the sorted side of the array for searching the target value.
-
-    // 1.1. Below condition assures us, the target element is in the left sorted array half.
-    if (searchN > arr[end]) {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end] && searchN > arr[mid]) start = mid + 1;
-      else if (arr[mid] < arr[end]) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-    // 1.2. Below condition assures us, the target element is in the right sorted array half.
-    else {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end]) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] < searchN) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] > searchN) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-  }
-
-  // Didn't found the target value within the array.
-  return -1;
+  
 }
