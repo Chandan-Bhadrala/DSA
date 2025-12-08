ea84604b35c8848f4a41982bb2822f9812199982
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-09 01:32:57 +0530

    Setting up files for PRAT ROTI and Beautiful Triplets.

--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
@@ -1,6 +1,7 @@
 // # PRATA ROTI
-// A beautiful description and the solution for this problem can be found on the below url.
+// A beautiful description and the solution for this problem can be found on the below urls.
 // https://copyassignment.com/roti-prata-spoj-problem-solution/
+// https://github.com/Ashutosh2128/Consistent-Coding-Daily-DSA/blob/main/Day-20/PRATA-RotiPrata.cpp
 //-----------------
 
 // 01. A function to solve for PRATA ROTI problem.
