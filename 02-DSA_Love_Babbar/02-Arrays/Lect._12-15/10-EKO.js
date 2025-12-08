63655bb57ca07cfa1ddf1622997bbf685298e69f
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-08 17:12:46 +0530

    EKO. Done.

--- 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/10-EKO.js
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/10-EKO.js
@@ -1,54 +1,54 @@
 // # Search for an element in a rotated and sorted array.
-// 1. Will be calculating pivot value first in this approach.
-// 1.1. And then will confirm at which side the target value lies and will scan for the target value in that range later using binary search.
-
-// Didn't tried this approach.
-// Happily learned about the fact that:
-// 1. Compare the start and the end pointer with the **mid pointer**.
-// 1.1. Role of the **mid-pointer**, is only of the comparison with the start or the end pointer, to make the right decision of shrinking the boundaries.
-// 2. In the unsorted array, always first find which half is sorted and only use the boundary pointer of the sorted half of the array to compare with it with the mid pointer, to come to the decision of shifting the start or the end pointer.
-// 2.1. Start and the end pointers are considered as the boundary pointers.
-// 2.1.1. Because they define the search space boundary.
+// 1. A woodcutter needs a wood out of a forest and he got trees length stored in an array.
+// 2. If he needs M metre of wood in total, he need to know at what height he must set his wood cutting blade to accumulate his required wood without over cutting any given tree.
 //-----------------
 
-// 01. A function to search for a given element in the rotated sorted array.
-function pivotElement(arr, searchN) {
+// 01. A function to find the blade height to cut the trees at the minimum possible height to not over-cut any single tree.
+function bladeHeight(arr, woodRequired) {
+  // Below, "start and end" blade height, decides the amount of tree cutting.
+  // Start = 0, means cut all trees full length.
   let start = 0;
-  let end = arr.length - 1;
-  let mid = null;
+  // end = Maximum height of the tree, means cut no tree at all.
+  let end = Math.max(...arr);
+
+  // Declaring and initializing the finalAnswer.
+  let finalAnswer = -1;
+
+  let maxWoodAvailable = arr.reduce(
+    (acc, currentValue) => acc + currentValue,
+    0
+  );
+
+  // Early return, if required wood is more than available wood.
+  if (woodRequired > maxWoodAvailable) return finalAnswer;
 
   while (start <= end) {
-    // Updating mid-pointer value.
-    mid = Math.floor((start + end) / 2);
+    // Updating mid-pointer value, to recalculate the blade height to chop down only required amount of wood.
+    let mid = Math.floor((start + end) / 2);
 
-     //00. If the element is found at mid.
-    if (arr[mid] === searchN) return mid;
-
-    // 1. Selecting either of the sorted side of the array for searching the target value.
-
-    // 1.1. Below condition assures us, the target element is in the left sorted array half.
-    if (searchN > arr[end]) {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end] && searchN > arr[mid]) start = mid + 1;
-      else if (arr[mid] < arr[end]) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-    // 1.2. Below condition assures us, the target element is in the right sorted array half.
-    else {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end]) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] < searchN) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] > searchN) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
+    if (isPossible(arr, mid, woodRequired)) {
+      // Increasing the mid value to try for another higher value of the mid (i.e., blade height), in pursuit to cut less of a trees.
+      start = mid + 1;
+      finalAnswer = mid;
+    } else {
+      // If current mid value is unable to accumulate the required amount of tree, choose lower blade height in pursuit to cut more of a single tree. To get the required amount of the wood out of trees.
+      end = mid - 1;
     }
   }
 
-  // Didn't found the target value within the array.
-  return -1;
+  // Returning the blade height
+  return finalAnswer;
+}
+
+function isPossible(arr, mid, woodRequired) {
+  let woodAccumulated = 0;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] > mid) {
+      woodAccumulated = woodAccumulated + arr[i] - mid;
+      if (woodAccumulated >= woodRequired) return true;
+    }
+    i++;
+  }
+  return false;
 }
