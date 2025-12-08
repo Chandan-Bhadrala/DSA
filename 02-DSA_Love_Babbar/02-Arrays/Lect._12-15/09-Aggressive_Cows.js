5087131c6fdd3c41cc1cee556c3167c91c3fb956
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-08 10:46:54 +0530

    Aggressive Cows. Part_1.

--- 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/09-Aggressive_Cows.js
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/09-Aggressive_Cows.js
@@ -1,46 +1,62 @@
 // # Aggressive Cows.
-// 1. 
+// 1. We have to assign stalls to the cows such that minimum distance between the cows is maximum.
 
 //-----------------
 
-// 01. A function to search for a given element in the rotated sorted array.
-function pivotElement(arr, searchN) {
-  let start = 0;
-  let end = arr.length - 1;
-  let mid = null;
+// 01a. A function to search for a mid value that can represent the maximum possible book pages that can be allotted to a student, which in contrast is the minimum possible book pages that have to be allotted to a student.
+function allocateStalls(arr, cows) {
+  // Early return.
+  if (students > arr.length || students <= 0) {
+    return -1; // As number of cows can't be greater than number of available stalls.
+  }
+
+  // Sorting stalls distance.
+  arr.sort((a, b) => a - b);
+
+  // Keeping start value instead of zero, rather equal to the minimum distance between the stalls, as the distance can't get any less than it.
+  let start = Math.min(...arr);
+  let end = arr.reduce((acc, current) => acc + current, 0);
+  let finalResult = -1;
 
   while (start <= end) {
-    // Updating mid-pointer value.
-    mid = Math.floor((start + end) / 2);
+    let mid = Math.floor((start + end) / 2);
 
-     //00. If the element is found at mid.
-    if (arr[mid] === searchN) return mid;
+    // Calling the isPossible function to check whether the given mid value represents the maximum possible pages that can be allotted to a student without violating the condition that no student must get more than the maximum possible book pages.
+    if (isPossible(arr, mid, cows)) {
+      // If true, then store the recent possible answer given by the mid in a variable.
+      finalResult = mid;
 
-    // 1. Selecting either of the sorted side of the array for searching the target value.
-
-    // 1.1. Below condition assures us, the target element is in the left sorted array half.
-    if (searchN > arr[end]) {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end] && searchN > arr[mid]) start = mid + 1;
-      else if (arr[mid] < arr[end]) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
-    }
-    // 1.2. Below condition assures us, the target element is in the right sorted array half.
-    else {
-      // Selecting the new index values to point, for the start and the end index.
-      if (arr[mid] > arr[end]) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] < searchN) start = mid + 1;
-      else if (arr[mid] < arr[end] && arr[mid] > searchN) end = mid - 1;
-
-      // Returning the target index, if found.
-      if (searchN == arr[start]) return start;
-      if (searchN == arr[end]) return end;
+      // Now, further reduce the search space to the left, to check for another reduced possible mid value.
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
   }
 
-  // Didn't found the target value within the array.
-  return -1;
+  // Returning the found answer for the maximum possible number of book pages that can be alloted to a student, which in contrast is a minimum possible book pages.
+  return finalResult;
+}
+
+// 01b. isPossible, a helper function to validate the viability of the mid value for a possible answer.
+function isPossible(arr, mid, cows) {
+  let cowsCount = 1;
+  let stallsDistance = 0;
+  let i = 0;
+
+  // Traversing the whole array.
+  while (i < arr.length) {
+   
+
+    if (stallsDistance + arr[i] <= mid) {
+      stallsDistance += arr[i];
+    } else {
+      // If the current student has reached its maximum possible pages, then bring up the next student and hand him over the next set of pages.
+      cowsCount++;
+      if (cowsCount > cows) return false;
+      stallsDistance = arr[i];
+    }
+    // Bring up the next book and its pages.
+    i++;
+  }
+  return true;
 }
