b739a2daa6f0501e711c69ebb245ba3288056906
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-08 15:42:39 +0530

    Aggressive Cows. Part_2. Done.

--- 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/09-Aggressive_Cows.js
+++ 02-DSA_Love_Babbar/02-Arrays/Lect._12-15/09-Aggressive_Cows.js
@@ -1,62 +1,58 @@
 // # Aggressive Cows.
 // 1. We have to assign stalls to the cows such that minimum distance between the cows is maximum.
+// 1.1. We will be given an array representing the stall numbers, we have to assign stalls two cows such that minimum distance between the two cows stalls is maximum possible.
 
 //-----------------
 
-// 01a. A function to search for a mid value that can represent the maximum possible book pages that can be allotted to a student, which in contrast is the minimum possible book pages that have to be allotted to a student.
+// 01a. A function to allocate the stalls to the cows with minimum distance between the two cows being maximum.
 function allocateStalls(arr, cows) {
   // Early return.
-  if (students > arr.length || students <= 0) {
-    return -1; // As number of cows can't be greater than number of available stalls.
+  if (cows > arr.length || cows <= 0) {
+    return -1; // As number of cows can't be greater than number of available stalls or a negative number.
   }
 
   // Sorting stalls distance.
   arr.sort((a, b) => a - b);
 
-  // Keeping start value instead of zero, rather equal to the minimum distance between the stalls, as the distance can't get any less than it.
-  let start = Math.min(...arr);
-  let end = arr.reduce((acc, current) => acc + current, 0);
+  // Keeping start value equal to stall 1.
+  let start = 1;
+  // Keeping end value equal to stall co-ordinate of the last stall - coordinate of the first stall.
+  let end = arr[arr.length - 1] - arr[0];
   let finalResult = -1;
 
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
 
-    // Calling the isPossible function to check whether the given mid value represents the maximum possible pages that can be allotted to a student without violating the condition that no student must get more than the maximum possible book pages.
+    // Calling the isPossible function to check whether the given mid value represents the maximum distance between the two cows stalls or not.
     if (isPossible(arr, mid, cows)) {
       // If true, then store the recent possible answer given by the mid in a variable.
       finalResult = mid;
 
-      // Now, further reduce the search space to the left, to check for another reduced possible mid value.
+      // Now, further reduce the search space to the right, to check for another bigger possible mid value.
       start = mid + 1;
     } else {
       end = mid - 1;
     }
   }
 
-  // Returning the found answer for the maximum possible number of book pages that can be alloted to a student, which in contrast is a minimum possible book pages.
+  // Returning the found answer for the maximum possible stall distance between the two cows.
   return finalResult;
 }
 
 // 01b. isPossible, a helper function to validate the viability of the mid value for a possible answer.
 function isPossible(arr, mid, cows) {
   let cowsCount = 1;
-  let stallsDistance = 0;
+  let lastStall = arr[0];
   let i = 0;
 
   // Traversing the whole array.
   while (i < arr.length) {
-   
-
-    if (stallsDistance + arr[i] <= mid) {
-      stallsDistance += arr[i];
-    } else {
-      // If the current student has reached its maximum possible pages, then bring up the next student and hand him over the next set of pages.
+    if (arr[i] - lastStall >= mid) {
+      lastStall = arr[i];
       cowsCount++;
-      if (cowsCount > cows) return false;
-      stallsDistance = arr[i];
+      if (cowsCount == cows) return true;
     }
-    // Bring up the next book and its pages.
     i++;
   }
-  return true;
+  return false;
 }
