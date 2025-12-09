adb1e9a1d4daab6d37ff041128cb40c4442e72a6
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-10 00:30:52 +0530

    Beautiful Triplets. Done.

--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/12-Beautiful_Triplets.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/12-Beautiful_Triplets.js
@@ -1,9 +1,10 @@
 // # Beautiful Triplets.
-// 1. In this question we will be given an array with the numbers in the increasing orders, we have to find count of beautiful triplets.
+// 1. In this question we will be given an array with the numbers in the increasing orders, we have to find **count** of beautiful triplets.
 // Q) What is beautiful triplets.
 // 1. If we have three numbers x, y, z.
 // 1.1. Then for these three numbers to be a beautiful triplets:
-// x - y == z - y == d.
+// x - y == z - y == d or
+// y = x + d and z = x + 2d
 // 2. In question, you'll be given the value of "d" and you'd have to find the numbers in the array that satisfy the condition or difference of value "d".
 
 //-----------------
@@ -15,9 +16,31 @@
 
 // 01. A function to search for a beautiful triplets.
 function beautifulTriplets(arr, difference) {
+  let count = 0;
+  arr.sort((a, b) => a - b);
+
+  for (let num of arr) {
+    if (
+      binarySearch(arr, num, difference) &&
+      binarySearch(arr, num, 2 * difference)
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function binarySearch(arr, num, difference) {
   let start = 0;
   let end = arr.length - 1;
-  let mid = null;
 
-  
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] == num + difference) return true;
+    else if (arr[mid] > num + difference) end = mid - 1;
+    else start = mid + 1;
+  }
+
+  return false;
 }
