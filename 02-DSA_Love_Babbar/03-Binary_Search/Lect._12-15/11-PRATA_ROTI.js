55f2e30102821704443f44e7550ad7c9d2580390
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-09 17:16:07 +0530

    PRAT ROTI. Part_1.

--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
@@ -2,6 +2,12 @@
 // A beautiful description and the solution for this problem can be found on the below urls.
 // https://copyassignment.com/roti-prata-spoj-problem-solution/
 // https://github.com/Ashutosh2128/Consistent-Coding-Daily-DSA/blob/main/Day-20/PRATA-RotiPrata.cpp
+
+// Unable to solve this question:
+// This solution approach uses AP and I didn't get the solution exactly.
+// Asked ChatGPT, he too said this question belongs to the Competitive Programming and not to the real world Web Dev world.
+// As per ChatGPT, if I were already able to solve and understand the Book Allocation and EKO DSA problem, then that means I'm already good enough with Binary Search and can comfortably ignore the PRATA ROTI DSA question. As this solution approach will never touch my web dev needs unless I'm appearing for the interview for the FAANG Companies.
+// Any company below FAANG will not be asking ROTI PRATA question.
 //-----------------
 
 // 01. A function to solve for PRATA ROTI problem.
