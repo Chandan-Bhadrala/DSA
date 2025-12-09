2ac1df13adc9a0a6d59c5387194e304f0d621726
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-09 23:54:29 +0530

    PRATA ROTI. Part_3. Done.

--- 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
+++ 02-DSA_Love_Babbar/03-Binary_Search/Lect._12-15/11-PRATA_ROTI.js
@@ -5,12 +5,15 @@
 
 //-----------------
 // ## Solution Approach:
-// 1. Give max time to each individual cook/chef to cook prata and add their cooked prata's in a global total sum of prata's. 
-// 2. Each chef takes R*P time to cook a prata.
-// 2.1. For first prata chef with R rating will take R*1, then for second prata chef will take R*2 time.
-// 2.2. So, for a given max time we will check how many integer number of prata's each chef can make.
+// 00. We will be given an array of chefs with their ranks and number of pranthas need to be cooked.
+// 00b. We have to return **how much time** will it take for given number of chefs to cook those pranthas.
+// 1. Give max time to each individual cook/chef to cook prata and add their cooked prata's in a global total sum of pranthas.
+// 2. Each chef takes R*P time to cook a prantha.
+// 2.1. For first prantha chef with R rating will take R*1, then for second prantha chef will take R*2 time.
+// 2.2. So, for a given max time we will check how many integer number of pranthas each chef can make.
 // 3. If all chefs under while or for loop were able to cook the given prata's within the given max time then we will iterate again with lesser mid value (max allowed time).
 // 3.1. If chefs were unable then we will iterate again with higher amount of time.
+// 4. Will return the **minimum required time** to cook the given number of pranthas by the given number of chefs.
 //-----------------
 
 // ## If unable to solve this question:
@@ -20,11 +23,52 @@
 // Any company below FAANG will not be asking ROTI PRATA question.
 //-----------------
 
-// 01. A function to solve for PRATA ROTI problem.
-function minimumTimeRequired(arr, searchN) {
-  let start = 0;
-  let end = arr.length - 1;
-  let mid = null;
+// 01a. A function to solve for PRATA ROTI problem, it will return the minimum required time to cook the given amount of the Rotis.
+function minimumTimeRequired(arr, requiredRotis) {
+  let startTimeBoundary = 0;
 
-  
+  // Used slowest cook rank and AP (Arithmetic Progression) to calculate the endTime boundary, "maxRank * AP"
+  let maxRank = Math.max(...arr);
+  let endTimeBoundary = maxRank * ((requiredRotis * (requiredRotis + 1)) / 2);
+
+  let finalAnswer = null;
+
+  // Run Binary Search on the upper and the lower bounds to narrow down for the possible result/answer.
+  while (startTimeBoundary <= endTimeBoundary) {
+    let mid = Math.floor((startTimeBoundary + endTimeBoundary) / 2);
+
+    if (isPossible(arr, mid, requiredRotis)) {
+      endTimeBoundary = mid - 1;
+
+      // Store and update the success case mid result.
+      finalAnswer = mid;
+    } else {
+      startTimeBoundary = mid + 1;
+    }
+  }
+  return finalAnswer;
+}
+
+// 01b. A helper function for checking whether the required Roti's can be cooked within the minTime (mid) or not. Thus, it returns a boolean.
+function isPossible(arr, mid, requiredRotis) {
+  let totalRotis = 0;
+
+  // Running for-loop for each individual chef "r" to see how many pranthas they can cook in the given maximum time.
+  for (let r of arr) {
+    // Declaring and initializing the rotis cooked and timeTaken for each chef from the beginning.
+    let roti = 1;
+    let timeTaken = roti * r;
+
+    // Running while loop for individual chef till chef takes the maximum allowed time (mid).
+    while (timeTaken <= mid) {
+      // updating totalRoti count.
+      totalRotis++;
+      if (totalRotis >= requiredRotis) return true;
+
+      // Calculating timeTaken for the next roti and updating the totalTimeTaken.
+      roti++;
+      timeTaken += roti * r;
+    }
+  }
+  return false;
 }
