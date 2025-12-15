057e26a2df8ad161c133196a29265e5d29cf9a73
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-15 23:17:04 +0530

    Check if rotated-sorted. Done.

--- 02-DSA_Love_Babbar/04-Sorting_Algorithms/Lect._16-21/09-Add_Two_Arrays.js
+++ 02-DSA_Love_Babbar/04-Sorting_Algorithms/Lect._16-21/09-Add_Two_Arrays.js
@@ -1,13 +1,20 @@
 /**
-# Check if rotated and sorted array.
+# Add two numbers represented as the array elements.
+Links:
+1. https://www.geeksforgeeks.org/problems/add-two-numbers-represented-by-two-arrays2408/1
+2. https://leetcode.com/problems/add-to-array-form-of-integer/description/
+
 ## Question:
-  1. We will be given an array and we have to check whether the given array is rotated and sorted or not.
-  2. We have to return a boolean.
+  1. We will be given two numbers whose digits are stored as an array elements in the arr1 and arr2.
+  2. We have to return the sum of those two numbers represented as the array elements.
 ## Solution Approach:
   1. 
  */
-function isRotatedSortedArray(arr) {
-  let isRotatedSorted = false;
-  
-  return isRotatedSorted;
+function calc_sum(arr1, arr2) {
+  let sum = 0;
+
+  for (let i = 0; i < arr2.length; i++) {
+    
+  }
+  return sum;
 }
