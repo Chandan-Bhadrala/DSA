fa40791310577ea79c0f5e3bece3a3650024fc4c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-15 15:33:52 +0530

    Rotated Sorted Array with the space complexity of O(n) is done.

--- 02-DSA_Love_Babbar/04-Sorting_Algorithms/Lect._16-21/07a-Rotate_Array.js
+++ 02-DSA_Love_Babbar/04-Sorting_Algorithms/Lect._16-21/07a-Rotate_Array.js
@@ -13,8 +13,25 @@
 ## Time Complexity: O(n)
 ## Space Complexity: O(n), as rotation has happened in a duplicate/copy array.
  */
-function rotateArray(arr) {
-  let rotatedArray = [...arr];
+function rotateArray(nums, k) {
+  const rotatedArray = [];
 
-  return rotatedArray;
+  // Re-calculating the value of k, to ensure k is smaller than the size of the array.
+  // If the given value of k is greater than the array-indices-length.
+  // Then, below expression will reduce the size of k into a reasonable number, which will be smaller than the nums.length.
+  // k: indicates the number of rotation or shifting needs to be done on the array elements.
+  k = k % nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    // Shifting ith element by k steps.
+    // To keep shifting within the array index range, we are using mod operator to produce the correct starting index for the overflown (i + k) indices.
+    let newIndex = (i + k) % nums.length;
+    // Adding array elements to the new shifted location.
+    rotateArray[newIndex] = nums[i];
+  }
+
+  // Changing the nums array values/elements in-place.
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = rotatedArray[i];
+  }
 }
