d447faacbedbd6fe8928322ff98fe6c7160ff39a
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-17 11:10:52 +0530

    Valid Palindrome. Done.

--- 02-DSA_Love_Babbar/05-Strings/Lect._22/04-Valid_Palindrome.js
+++ 02-DSA_Love_Babbar/05-Strings/Lect._22/04-Valid_Palindrome.js
@@ -5,33 +5,57 @@ https://leetcode.com/problems/valid-palindrome/description/
 
 ## Question:
 1. Need to check given string is a valid palindrome or not.
-2. Has to remove the alphanumeric characters from the string and perform the palindrome check.
+2. Has to remove the non-alphanumeric characters from the string and perform the palindrome check.
 ## Solution Approach:
-  1. Use replace and regex function to remove the alphanumeric characters from the string.
+  1. Use replace and regex function to remove the non-alphanumeric characters from the string.
+    1.1. Or use if-condition to push only **a-z or A-Z or 1-9** in the new array and **ignore** all other non-alphanumeric characters.
   2. Then make the palindrome check. 
 */
 
 // -----------------------------
 
-// 01. A function implementation of the Selection Sort.
-// 1. Selection Sort, swaps array elements only once.
-function selectionSort(arr) {
-  let sortArr = [...arr];
-
-  // Take first element of the array and then compare it with other array elements of the array using inner-loop.
-  // If any other array element of the inner-loop found bigger than the array element selected by the outer-loop then, save its index and swap those elements **ONLY ONCE** at the end of the inner-loop only, to save CPU cycles on swapping.
-  for (let i = 0; i < sortArr.length; i++) {
-    let minimumIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (sortArr[minimumIndex] > sortArr[j]) {
-        // Way to swap array elements in JS.
-        minimumIndex = j;
-      }
+// 00. A helper function to remove all the non-alphanumeric character array elements from the passed characters-array.
+function removeNonAlphanumericChar(s) {
+  let newString = "";
+  for (let i = 0; i < s.length; i++) {
+    if (
+      (s[i] >= "a" && s[i] <= "z") ||
+      (s[i] >= "A" && s[i] <= "Z") ||
+      (s[i] >= "0" && s[i] <= "9")
+    ) {
+      newString = newString + s[i];
     }
-    // Swap Once.
-    // Swap the found minimum index with the help of the inner-loop, with the outer-loop index value.
-    [sortArr[i], sortArr[minimumIndex]] = [sortArr[minimumIndex], sortArr[i]];
+  }
+  console.log(newString);
+  return newString;
+}
+
+// 01. A function to check for the palindrome.
+function validPalindrome(s) {
+  // Converting "s" into a lowercase and further Splitting string "s" into an array elements.
+  s = s.toLowerCase().split("");
+
+  // Calling helper function to remove non-alphanumeric characters.
+  s = removeNonAlphanumericChar(s);
+
+  let sCopy = s.split("");
+
+  let i = 0;
+  let j = sCopy.length - 1;
+
+  while (i < j) {
+    [sCopy[i], sCopy[j]] = [sCopy[j], sCopy[i]];
+    i++;
+    j--;
   }
 
-  return sortArr;
+  // Joining/Converting sCopy as an array elements back to a string.
+  sCopy = sCopy.join("");
+
+  return s == sCopy;
 }
+
+//------------
+let s = "A man, a plan, a canal: Panama";
+
+console.log("Check Palindrome:", validPalindrome(s));
