1b9a30433b440caa1addeda1c40d4385d5891ca1
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-18 17:37:55 +0530

    Setting up files.

---
+++ 02-DSA_Love_Babbar/05-Strings/Lect._22/09a-Remove_All_Occurrences_of_Substring.js
@@ -0,0 +1,18 @@
+/**
+# Remove all occurrences of a substring.
+
+Link:
+https://leetcode.com/problems/remove-all-occurrences-of-a-substring/description/
+
+## Question:
+1. Find the number of characters in the string.
+## Solution Approach:
+
+*/
+
+// -----------------------------
+
+// 01. A function implementation to remove all occurrences of a substring - using replace helper function.
+function removeAllSubstrings(s, part) {
+ 
+}
