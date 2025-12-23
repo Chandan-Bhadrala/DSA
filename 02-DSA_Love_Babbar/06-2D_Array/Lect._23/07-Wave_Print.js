9c8574dc6c56abaf627bac98ff9e316cdc222a91
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-23 17:22:51 +0530

    Wave Print. Done.

--- 02-DSA_Love_Babbar/06-2D_Array/Lect._23/07-Wave_Print.js
+++ 02-DSA_Love_Babbar/06-2D_Array/Lect._23/07-Wave_Print.js
@@ -1,7 +1,7 @@
 /**
 # Print array elements in a wave fashion.
 Link:
-https://leetcode.com/problems/sum-in-a-matrix/description/
+https://www.naukri.com/code360/problems/print-like-a-wave_893268?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_6&leftPanelTabValue=PROBLEM&customSource=studio_nav
 ## Question:
 ## Solution Approach:
   1. 
@@ -9,7 +9,26 @@ https://leetcode.com/problems/sum-in-a-matrix/description/
 // -----------------------------
 
 // ## Print array elements in a wave fashion.
-function wavePrint(matrix) {}
+function wavePrint(arr) {
+  let rows = arr.length;
+  let cols = arr[0].length;
+
+  let resultArr = [];
+  // Traverse array column wise and push the array elements into the new array as per question requirements.
+
+  for (let j = 0; j < cols; j++) {
+    if (j % 2 == 0) {
+      for (let i = 0; i < rows; i++) {
+        resultArr.push(arr[i][j]);
+      }
+    } else {
+      for (let i = rows - 1; i >= 0; i--) {
+        resultArr.push(arr[i][j]);
+      }
+    }
+  }
+  return resultArr;
+}
 
 // --- Output:
 console.log(
