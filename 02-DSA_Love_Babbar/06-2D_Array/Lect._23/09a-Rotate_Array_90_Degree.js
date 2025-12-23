b34f5ae62be9480e295fb3bae95382dd69842f8f
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-24 01:56:30 +0530

    Setting up files.

--- 02-DSA_Love_Babbar/06-2D_Array/Lect._23/09a-Rotate_Array_90_Degree.js
+++ 02-DSA_Love_Babbar/06-2D_Array/Lect._23/09a-Rotate_Array_90_Degree.js
@@ -12,20 +12,36 @@ https://leetcode.com/problems/rotate-image/
 function rotateArrayBy90(matrix) {
   let rows = matrix.length;
   let cols = matrix[0].length;
-  
-  let resultantArr=[];
-  
-  resultantArr[][]= matrix[][]
+
+  // Defining boundaries.
+  let right = matrix[0].length - 1;
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let i = 0;
+
+  let resultantArr = [];
+
+  // Define number of rows of the resultantArr before accessing them.
+  for (let i = 0; i < rows; i++) {
+    resultantArr[i] = [];
+  }
+
+  while (right >= 0) {
+    for (let k = top, j = 0; k <= bottom; k++, j++) {
+      resultantArr[k][right] = matrix[i][j];
+    }
+    right--;
+    i++;
+  }
+  return resultantArr;
 }
 
 // --- Output:
 console.log(
   "Rotate given array by 90 degrees:",
-  rotateArrayBy90(
-    [
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-    ]
-  )
+  rotateArrayBy90([
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+  ])
 );
