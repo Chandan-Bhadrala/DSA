4c1d3be31aa23c1da74a7df6164f9e50fa878709
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-23 11:03:02 +0530

    Setting up files.

---
+++ 02-DSA_Love_Babbar/06-2D_Array/Lect._23/06b-Sum_in_Matrix.js
@@ -0,0 +1,27 @@
+/**
+# Sum in a Matrix.
+Link:
+https://leetcode.com/problems/sum-in-a-matrix/description/
+## Question:
+## Solution Approach (Sort and remove) - Time Complexity: O(mn log n).
+  1. 
+
+*/
+// -----------------------------
+
+// ## Find Sum in a Matrix.
+function sumMatrix(matrix, target) {
+  
+}
+
+// --- Output:
+console.log(
+  "Sum in Matrix ",
+  findTarget(
+    [
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ]
+  )
+);
