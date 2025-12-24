064c085867123dfbe94cdcc9bc45b1ad94fb018c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-24 11:29:38 +0530

    Staircase search. Done.

--- 02-DSA_Love_Babbar/06-2D_Array/Lect._23/10-Search_2D_Matrix.js
+++ 02-DSA_Love_Babbar/06-2D_Array/Lect._23/10-Search_2D_Matrix.js
@@ -1,5 +1,5 @@
 /**
-# Rotate Array by 90 degree.
+# Find Target. Level II. Staircase Search.
 Link:
 https://leetcode.com/problems/search-a-2d-matrix-ii/
 ## Question:
@@ -10,17 +10,29 @@ https://leetcode.com/problems/search-a-2d-matrix-ii/
 
 // ## Staircase Search the target in a 2D array. Time Complexity: O(m + n)
 function findTarget(matrix, target) {
-  let rows = matrix.length;
-  let cols = matrix[0].length;
+  // Define boundary values.
+  let rows = matrix.length - 1;
+  let cols = matrix[0].length - 1;
 
-  // Iterate row iterator to access matrix[i][j] properly.
-  for (let i = 0; i < rows; i++) {
-    // Iterate col iterator to access matrix[i][j] properly.
-    for (let j = 0; j < cols; j++) {
-      if (matrix[i][j] == target) return true;
+  // Starting value of the iterators.
+  let i = 0;
+  let j = cols;
+
+  // Used && operator instead of the || operator.
+  // As the 2D dimension search space will collapse immediately the moment one iterator becomes negative or goes out of bound.
+  while (j >= 0 && i <= rows) {
+    if (matrix[i][j] == target) {
+      console.log(i, j);
+      return true;
+    }
+
+    // Move iterators in the stair case fashion.
+    if (matrix[i][j] > target) {
+      j--;
+    } else {
+      i++;
     }
   }
-
   // Looped out of the matrix, i.e., never found the target value.
   return false;
 }
@@ -36,6 +48,6 @@ console.log(
       [10, 13, 14, 17, 24],
       [18, 21, 23, 26, 30],
     ],
-    3
+    9
   )
 );
