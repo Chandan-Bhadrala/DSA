dea1cef748e64301ced2ff0416aba06147a7f064
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-12-31 19:10:09 +0530

    Trying 09) Sum of Digits.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/08-Count_Digits.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/08-Count_Digits.js
@@ -7,8 +7,18 @@
 // -----------------------------
 
 function countDigits(n) {
+  // Base Case: n < 0. Base case will stop recursion-loop and seed initial count value.
+  if (n <= 0) return 0;
 
+  return countDigits(Math.floor(n / 10)) + 1;
 }
 
 // --- Output:
-console.log(factorial(10));
+console.log(countDigits(99));
+
+/**
+f(0) = 0
+f(1) = return f(0) + 1;
+f(2) = return f(1) + 1;
+f(3) = return f(2) + 1;
+*/
--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/09-Sum_of_Digits.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/09-Sum_of_Digits.js
@@ -3,12 +3,35 @@
 
 ## Question:
 ## Solution Approach:
+Requirement: I need recursive call to either return sum or the digits.
+1. If I make recursion call to return sum, then I can return that sum to the main calling function too.
 */
 // -----------------------------
 
-function factorial(n) {
-  
+function sum_of_digits(n, sum = 0) {
+  // Base Case: Terminate the recursive loop and seed initial value for sum-> 0.
+  if (n <= 0) {
+    return;
+  }
+
+  // Collect digits to add to the sum.
+  let digits = sum_of_digits(Math.floor(n / 10));
+
+  // Add last digit to the sum
+  return (sum += digits % 10);
 }
 
 // --- Output:
-console.log(factorial(10));
+console.log(sum_of_digits(779));
+
+/**
+Stacking
+f(77)
+f(7) 
+f(0)
+---
+Unwinding
+ 
+f(0) = Nan;
+f(7) = f(0) + 
+*/
