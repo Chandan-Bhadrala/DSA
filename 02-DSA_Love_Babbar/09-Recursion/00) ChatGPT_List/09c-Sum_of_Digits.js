7e8138b527998edc0657ff08bc7a28d86514b497
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-01 20:15:42 +0530

    Sum of Digits. Done.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/09c-Sum_of_Digits.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/09c-Sum_of_Digits.js
@@ -3,23 +3,35 @@
 
 ## Question:
 ## Solution Approach:
-Requirement: I need recursive call to either return sum or the digits.
-1. If I make recursion call to return sum, then I can return that sum to the main calling function too.
+Requirement: I've to do three things:
+1. Break n in every passing recursion.
+2. Need to mod out last digit.
+3. Need add the digits
+4. Finally return the sum.
+  4.1. As I've to finally return the sum then every recursion must be returning the same variable that is sum.
+    4.1.1. So, one requirement sum is fixated on the return.
+  4.2. Next I need to break the n.
+    4.2.1. That I'll be doing within the passing argument of the recursion calls.
+  4.3. Last I need to extract the last digit of the n.
+    4.3.1. That I'll be doing in the business logic area.
 */
 // -----------------------------
 
 // Trial 3:
-function sum_of_digits(n, sum = 0) {
+function sum_of_digits(n) {
   // Base Case: Terminate the recursive loop and seed initial value for sum-> 0.
   if (n <= 0) {
-    return;
+    return 0;
   }
 
-  // Collect digits to add to the sum.
-  let digits = sum_of_digits(Math.floor(n / 10));
+  // Collect seed sum value to add to the sum.
+  let lastSum = sum_of_digits(Math.floor(n / 10));
+
+  // Extract last digit of the n.
+  // let lastDigit = n % 10; // Will extract and add the last digit below in one go in the return statement.
 
   // Add last digit to the sum
-  return (sum += digits % 10);
+  return lastSum + (n % 10);
 }
 
 // --- Output:
@@ -27,12 +39,15 @@ console.log(sum_of_digits(779));
 
 /**
 Stacking
-f(77)
-f(7) 
+f(779)
+f(77) 
+f(7)
 f(0)
 ---
 Unwinding
  
-f(0) = Nan;
-f(7) = f(0) + 
+f(0) = 0;
+f(7) = f(0) + 7;
+f(77) = f(7) + 7;
+f(779) = f(77) + 9; -> final return
 */
