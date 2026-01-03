aa8887332dad646cf283cbc98e61f68d11fab8a8
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-03 22:57:50 +0530

    Setting up files.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/18-Palindrome.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/18-Palindrome.js
@@ -7,7 +7,7 @@
 // -----------------------------
 
 // Linear Search
-function countOccurrences(arr, i = 0) {}
+function isPalindrome(arr, i = 0) {}
 
 // --- Output:
-console.log(countOccurrences([1, 2, 2, 2, 3, 3, 4, 4, 7, 7, 7, 9, 9]));
+console.log(isPalindrome("WOW"));
