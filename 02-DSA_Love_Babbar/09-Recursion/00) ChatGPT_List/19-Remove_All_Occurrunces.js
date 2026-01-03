0c14d0fa85eefac667a95890e179826e36d4aae8
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-03 23:24:38 +0530

    Setting up files.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/19-Remove_All_Occurrunces.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/19-Remove_All_Occurrunces.js
@@ -7,7 +7,7 @@
 // -----------------------------
 
 // Linear Search
-function countOccurrences(arr, i = 0) {}
+function removeOccurrences(str, target, i = 0) {}
 
 // --- Output:
-console.log(countOccurrences([1, 2, 2, 2, 3, 3, 4, 4, 7, 7, 7, 9, 9]));
+console.log(countOccurrences("aabbddcdeffxxfzzzzgdfldf", "x"));
