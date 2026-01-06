dee14389c2e6304991d01f5c6a354d5abb6fa28d
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-06 11:47:31 +0530

    Fibonacci. Done.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/23a-Fibonacci_Number.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/23a-Fibonacci_Number.js
@@ -18,3 +18,23 @@ function FibonacciNumber(n, i = 0, n1 = 0, n2 = 1, sum = n1 + n2) {
 
 // --- Output:
 console.log(FibonacciNumber(9));
+
+/**
+## While Stacking
+Frame Stack 1:
+n=9, i=0, n1=0, n2=1, sum=1;
+
+Frame Stack 2:
+n=9, i=1, n1=1, n2=1, sum=prevn1 + prevn2; // Sum has previous value of n1 & n2 rather than new updated values of n1 & n2.
+// That's why the error
+
+Frame Stack 3:
+n=9, i=0, n1=0, n2=1, sum=;
+
+---
+## While Unwinding
+Frame Stack 3:
+Frame Stack 2:
+Frame Stack 1:
+
+*/
--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/24-Print_Fibonacci_Series.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/24-Print_Fibonacci_Series.js
@@ -7,7 +7,14 @@
 // -----------------------------
 
 // Linear Search
-function printFibonacciSeries(n) {}
+function printFibonacciSeries(n, i = 0, n1 = 0, n2 = 1) {
+  // Base Case: Terminates the recursive loop when iterator i == n.
+  if (i == n) return n1;
+
+  console.log(n1);
+  // Return accumulated value of sum, at the time base case gets hits.
+  return printFibonacciSeries(n, i + 1, n2, n1 + n2);
+}
 
 // --- Output:
 console.log(printFibonacciSeries(9));
