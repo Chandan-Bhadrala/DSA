db2a33434e31dce4a63d5602598afde5736db371
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-06 15:49:43 +0530

    Setting up files.

--- 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/31-Search_Rotate_Sorted_Array.js
+++ 02-DSA_Love_Babbar/09-Recursion/00) ChatGPT_List/31-Search_Rotate_Sorted_Array.js
@@ -6,8 +6,8 @@
 */
 // -----------------------------
 
-// Linear Search
-function countOccurrences(arr, i = 0) {}
+// Binary Search:
+function searchTarget(arr, target, i = 0) {}
 
 // --- Output:
-console.log(countOccurrences([1, 2, 2, 2, 3, 3, 4, 4, 7, 7, 7, 9, 9]));
+console.log(searchTarget([1, 2, 2, 2, 3, 3, 4, 4, 7, 7, 7, 9, 9], 7));
