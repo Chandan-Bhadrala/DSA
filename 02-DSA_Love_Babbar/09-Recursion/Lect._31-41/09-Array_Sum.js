ff352e94a278c8334a2b61b35147770386828c8c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-07 16:32:49 +0530

    Array Elements Sum. Done.

--- 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/09-Array_Sum.js
+++ 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/09-Array_Sum.js
@@ -6,7 +6,18 @@
 */
 // -----------------------------
 
-function sumArray(arr) {}
+function sumArray(arr, i = 0) {
+  // Base Case: To terminate the recursion and to seed a base value of 0.
+  if (i == arr.length) return 0;
+
+  // Building frame stack memory with the arr and respective value of the iterator i.
+  // Receiving the value of sum starting from the base value
+  let sum = sumArray(arr, i + 1);
+
+  // Building result upon the seed value received from the base case.
+  // Using frame stack memory to access the arr elements.
+  return (sum += arr[i]);
+}
 
 // --- Output:
 console.log(sumArray([1, 2, 3, 4, 5]));
