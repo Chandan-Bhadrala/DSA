4b3806804b7f0a5935435471c2d8d61a7987f8a8
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-07 13:56:17 +0530

    Onto Say Digits.

--- 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/07-Say_Digits.js
+++ 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/07-Say_Digits.js
@@ -6,9 +6,23 @@
 */
 // -----------------------------
 
+const digitsWordMap = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
 function sayDigits(n) {
- 
+  // Base Case: Let recursive calls build frame stack until n becomes 0 or less than 0.
+  if (n <= 0) return;
 }
 
 // --- Output:
-console.log(sayDigits(127))
+console.log(sayDigits(127));
