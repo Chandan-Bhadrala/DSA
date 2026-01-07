ead3dc5a1126504dfb3c7d95d6e590bc7260464c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-07 13:26:47 +0530

    Factorial. Done.

--- 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/02-Factorial_N.js
+++ 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/02-Factorial_N.js
@@ -7,7 +7,14 @@
 // -----------------------------
 
 function factorial(n) {
-  
+  // Base case: Seeds return value of 1.
+  if (n == 0) return 1;
+
+
+  // Initially we are creating frame stacks which only have memory of the value of n for them.
+  // And while unwinding we are using the return value of the deeper recursion to multiply it with the current value of n.
+  // We are doing so, till we reach to the original value of n and then finally multiply and return the value to the caller function.
+  return n * factorial(n - 1);
 }
 
 // --- Output:
