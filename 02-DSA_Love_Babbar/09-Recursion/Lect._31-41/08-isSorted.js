86e77ba7b729a3f606277bb051d8f20806228019
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-16 00:32:09 +0530

    isSorted. Done.

--- 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/08-isSorted.js
+++ 02-DSA_Love_Babbar/09-Recursion/Lect._31-41/08-isSorted.js
@@ -6,7 +6,16 @@
 */
 // -----------------------------
 
-function isSorted(arr) {}
+function isSorted(arr, n = arr.length - 1) {
+  // Base case: array of size 0 or 1 is sorted
+  if (n <= 1) return true;
+
+  // If last two elements are not in ascending order
+  if (arr[n - 2] > arr[n - 1]) return false;
+
+  // Recursive call for remaining array
+  return isSorted(arr, n - 1);
+}
 
 // --- Output:
-console.log(isSorted([1, 2, 3, 4, 5]));
+console.log(isSorted([1, 2, 3, 4, 7, 5]));
