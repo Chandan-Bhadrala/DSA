1b507eaa3bddf204cc544b18de9074fbfb88332a
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-17 11:44:56 +0530

    Onto maximum sub-array Sum.

--- 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/02a-Maximum_Subarray_Sum.js
+++ 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/02a-Maximum_Subarray_Sum.js
@@ -2,12 +2,16 @@
 # Find maximum subarray Sum.
 
 ## Question:
+1. Find a contiguous slice of an array that has the largest sum.
 ## Solution Approach:
 */
 // -----------------------------
 
 function maxSubArraySum(arr) {
-  
+  let maxSum = null;
+
+  for (let i = 0; i < arr.length; i++) {}
+  return maxSum;
 }
 
 // --- Output:
