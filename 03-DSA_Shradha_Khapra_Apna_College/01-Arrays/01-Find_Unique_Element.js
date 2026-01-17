9854ae025f8ab094d706e6f3e3e1376477acccce
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-01-17 11:33:22 +0530

    Find Unique Element. Done.

--- 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/01-Find_Unique_Element.js
+++ 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/01-Find_Unique_Element.js
@@ -7,15 +7,11 @@
 */
 // -----------------------------
 
-function waysToClimbStairs(n) {
-  // Base Case: for step = 0 or 1, we only have one way to reach to the step 0 or step 1.
-  if (n == 0 || n == 1) return 1;
-
-  // To reach the step n, we must be either at step n - 1 or n - 2.
-  // So, to reach the step n from the step n - 1 or n - 2. We must know in how many ways we have reached to the step n - 1 or n - 2.
-  // And finally the number of ways we can reach the step n is equal to the sum of the waysToClimbStairs(n - 1) + waysToClimbStairs(n - 2).
-  return waysToClimbStairs(n - 1) + waysToClimbStairs(n - 2);
+function uniqueElement(arr) {
+  let ans = null;
+  for (let x of arr) ans ^= x;
+  return ans;
 }
 
 // --- Output:
-console.log(waysToClimbStairs(7));
+console.log(uniqueElement([1, 2, 2, 3, 4, 3, 1, 5, 4]));
--- 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/02-Maximum_Subarray_Sum.js
+++ 03-DSA_Shradha_Khapra_Apna_College/01-Arrays/02-Maximum_Subarray_Sum.js
@@ -6,15 +6,9 @@
 */
 // -----------------------------
 
-function waysToClimbStairs(n) {
-  // Base Case: for step = 0 or 1, we only have one way to reach to the step 0 or step 1.
-  if (n == 0 || n == 1) return 1;
-
-  // To reach the step n, we must be either at step n - 1 or n - 2.
-  // So, to reach the step n from the step n - 1 or n - 2. We must know in how many ways we have reached to the step n - 1 or n - 2.
-  // And finally the number of ways we can reach the step n is equal to the sum of the waysToClimbStairs(n - 1) + waysToClimbStairs(n - 2).
-  return waysToClimbStairs(n - 1) + waysToClimbStairs(n - 2);
+function maxSubArraySum(arr) {
+  
 }
 
 // --- Output:
-console.log(waysToClimbStairs(7));
+console.log(maxSubArraySum([1, 2, -2, 3, 4, -3, 1, 5, 4]));
