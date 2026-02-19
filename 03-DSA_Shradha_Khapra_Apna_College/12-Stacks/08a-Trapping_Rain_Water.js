0f4918a09075267c76fcd37b5a24d52bc238c29e
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-02-19 14:11:03 +0530

    Onto Trapping Water.

--- 03-DSA_Shradha_Khapra_Apna_College/12-Stacks/08a-Trapping_Rain_Water.js
+++ 03-DSA_Shradha_Khapra_Apna_College/12-Stacks/08a-Trapping_Rain_Water.js
@@ -1,34 +1,20 @@
 /**
+# Trapping Rain Water.
+
+## Question:
+
+## Solution:
+1. We'll be calculating the left and the right bound for each bar.
+    1.1. Then select the smallerBoundary = min(leftBoundary, rightBoundary).
+2. Then water stored by each bar is equal to the smallerBoundary - heightOfBar.
+    2.1. This formula will give us the trapped water by each bar.
+3. At last we'll sum up water trapped by the each bar to conclude our final answer.
+*/
+
+/**
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    let totalWater = 0;
-    const stack = []; // Stores indices
-
-    for (let i = 0; i < height.length; i++) {
-        // While current bar is taller than the bar at the stack's top
-        // we've found a potential "right wall" for a container
-        while (stack.length > 0 && height[i] > height[stack[stack.length - 1]]) {
-            const bottomIndex = stack.pop(); // The "floor" of the container
-            
-            // If stack is empty, there is no left wall to hold water
-            if (stack.length === 0) break;
-            
-            const leftWallIndex = stack[stack.length - 1];
-            const rightWallIndex = i;
-            
-            // Calculate height of the water level (limited by shorter wall)
-            const boundedHeight = Math.min(height[leftWallIndex], height[rightWallIndex]) - height[bottomIndex];
-            
-            // Calculate width between the walls
-            const width = rightWallIndex - leftWallIndex - 1;
-            
-            totalWater += boundedHeight * width;
-        }
-        
-        stack.push(i);
-    }
-
-    return totalWater;
+var trap = function (height) {
+  
 };
