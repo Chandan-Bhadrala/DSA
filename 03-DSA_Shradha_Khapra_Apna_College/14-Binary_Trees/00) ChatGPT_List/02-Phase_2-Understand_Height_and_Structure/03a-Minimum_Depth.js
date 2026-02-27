9e1281881bcfa44c522050b315b38ec56620a38c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-02-27 15:24:06 +0530

    Onto Minimum Depth.

--- 03-DSA_Shradha_Khapra_Apna_College/14-Binary_Trees/00) ChatGPT_List/02-Phase_2-Understand_Height_and_Structure/03a-Minimum_Depth.js
+++ 03-DSA_Shradha_Khapra_Apna_College/14-Binary_Trees/00) ChatGPT_List/02-Phase_2-Understand_Height_and_Structure/03a-Minimum_Depth.js
@@ -14,13 +14,19 @@
 
 // We recurse till we find the **LEAF NODE** (a node with no/0 child).
 const minDepth = function (root) {
-  // Base case: We recurse till we find a node with no right and the left node. A true leaf node.
+  // Base case: We recurse till we find a node with no right and the left node. A true LEAF NODE.
   // Because a leaf node decides the min or the max height.
-  if (!root) return 0;
+  if (!root) return 0; // Recurse till leaf node is found and then only return a base value to compile answer on.
 
   if (!root.left) {
+    // Just recurse, if there is a right branch. To reach to the Leaf Node.
     minDepth(root.right);
   }
 
-  if()
+  if (!root.right) {
+    // Just recurse, if there is a left branch. Again just to reach to the Leaf Node.
+    minDepth(root.left);
+  }
+
+  return Math.min() + 1; // I've nothing now other than 0, to accumulate my answer on. +1 is for the parent node. How to accumulate children result.
 };
