cb715e3aa85fd3148e6c356f5583a035ada9ba3b
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-03-02 13:21:25 +0530

    Check if tree isSymmetric.

--- 03-DSA_Shradha_Khapra_Apna_College/14-Binary_Trees/00) ChatGPT_List/03-Phase_3-Combine_Left_and_Right/03a-Check_if_Tree_isSymmetric.js
+++ 03-DSA_Shradha_Khapra_Apna_College/14-Binary_Trees/00) ChatGPT_List/03-Phase_3-Combine_Left_and_Right/03a-Check_if_Tree_isSymmetric.js
@@ -11,10 +11,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isSymmetric = function(root) {
-   
+const isSymmetric = function (root) {
+  // Defensive check: For a given empty tree.
+  if (!root) return true;
+
+  return isMirror(root.left, root.right);
 };
 
-const isMirror = function(t1, t2) {
-   
+const isMirror = function (t1, t2) {
+  if (!t1 || !t2) return t1 == t2;
+
+  // Traversing to the concerned nodes at each recursive step to check for the isSymmetric property.
+  let ltCheck = isMirror(t1.left, t2.right);
+  let rtCheck = isMirror(t1.right, t2.left);
+
+  // Returning the isSymmetric result.
+  return ltCheck && rtCheck && t1.val == t2.val;
 };
